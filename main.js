0e9ded11c35d780735692b9a12862867736fe74d
Siddak-cloud <85190641+Siddak-cloud@users.noreply.github.com>
2021-10-12 10:25:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+quick_draw_data_set = ["pen","paper","book","bottle"];
+
+random_no = Math.floor((Math.random()*quick_draw_data_set.length)+1);
+  
+console.log(quick_draw_data_set [random_no]);
+
+sketch = quick_draw_data_set[random_no];
+
+document.getElementById("p3").innerHTML = 'Sketch to be drawn: '+sketch;
+
